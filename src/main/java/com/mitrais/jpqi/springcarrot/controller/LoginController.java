6945e7ef874024cb2977b52fee3d3533cce131f0
Reni_Kumalawati <reni_k901@mitrais.com>
2019-03-15 13:53:51 +0800

    pull dari dimas

--- src/main/java/com/mitrais/jpqi/springcarrot/controller/LoginController.java
+++ src/main/java/com/mitrais/jpqi/springcarrot/controller/LoginController.java
@@ -3,14 +3,12 @@ package com.mitrais.jpqi.springcarrot.controller;
 import com.mitrais.jpqi.springcarrot.model.Employee;
 import com.mitrais.jpqi.springcarrot.service.EmployeeServiceUsingDB;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/login")
 public class LoginController {
     @Autowired
