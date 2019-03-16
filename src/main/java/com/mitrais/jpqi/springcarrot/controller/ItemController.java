20fda035b3cf36a89adef7a469c739edc81f6654
latudimas <riswandha.ld@gmail.com>
2019-03-16 09:12:44 +0700

    add methods findbyCarrotAmount to list all rewards based on carrot amount

--- src/main/java/com/mitrais/jpqi/springcarrot/controller/ItemController.java
+++ src/main/java/com/mitrais/jpqi/springcarrot/controller/ItemController.java
@@ -62,4 +62,9 @@ public class ItemController {
     public List<Item> findByBazaarId(@RequestParam String id) {
         return itemService.findAllByBazaarId(id);
     }
+
+    @GetMapping("findByCarrotAmount")
+    public List<Item> findByCarrotAmount(@RequestParam int carrotAmount) {
+        return itemService.findAllByExchangeRateAmount(carrotAmount);
+    }
 }
