fe9eacf68b4cd7037dc90686413a916c02cb874e
Rafif Reyhandhia (Vendor) <v-rafif.reyhandhia@btpnsyariah.com>
2020-10-27 01:48:25 +0700

    Disable selenium test

--- src/test/java/com/mitrais/jpqi/springcarrot/TestCuy.java
+++ src/test/java/com/mitrais/jpqi/springcarrot/TestCuy.java
@@ -1,10 +1,10 @@
-package com.mitrais.jpqi.springcarrot;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({TestSelenium.class, TestSelenium2.class})
-public class TestCuy {
-}
-
+//package com.mitrais.jpqi.springcarrot;
+//
+//import org.junit.runner.RunWith;
+//import org.junit.runners.Suite;
+//
+//@RunWith(Suite.class)
+//@Suite.SuiteClasses({TestSelenium.class, TestSelenium2.class})
+//public class TestCuy {
+//}
+//
